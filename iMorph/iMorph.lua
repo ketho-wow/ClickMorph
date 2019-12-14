4117a100a231061f9a9c8622255dbd9e7322383c
Ketho <sghs17@outlook.com>
2019-12-14 01:15:57 +0100

    Implement morphing on login, inject, zoning Implement smart morphing

--- iMorph/iMorph.lua
+++ iMorph/iMorph.lua
@@ -3,14 +3,25 @@ if not CM.isClassic then return end
 local db
 local state
 
+local VERSION = 1
+local debug_mode
+local initTime
+
 local SEX_MALE = 1
 local SEX_FEMALE = 2
 
-local FLAG_SMARTMORPH = 2
+local EnchantSlots = {
+	[1] = INVSLOT_MAINHAND,
+	[2] = INVSLOT_OFFHAND,
+}
+
+--local FLAG_SMARTMORPH = 2
+local playerClass = select(2, UnitClass("player"))
+local canShapeshift = playerClass == "DRUID" or playerClass == "SHAMAN"
+local shapeshifted
 
 CM.override = true
 if CM.override then -- temporary dummy table
-	print("ClickMorph: overriding iMorph")
 	IMorphInfo = IMorphInfo or {
 		items = {},
 		enchants = {},
@@ -25,33 +36,101 @@ local iMorphLua = CreateFrame("Frame")
 _G.iMorphLua = iMorphLua
 
 function iMorphLua:OnEvent(event, ...)
-	C_Timer.After(0, function()	-- imorph api is not yet registered
+	self[event](self, ...)
+end
+
+iMorphLua:RegisterEvent("PLAYER_ENTERING_WORLD")
+iMorphLua:SetScript("OnEvent", iMorphLua.OnEvent)
+
+-- when imorph is already injected
+function iMorphLua:PLAYER_ENTERING_WORLD(isInitialLogin, isReloadingUi)
+	initTime = time()
+	C_Timer.After(0, function() -- imorph api is not yet registered
+		if GetClickMorph() then
+			print("ClickMorph: overriding iMorph")
+		end
+		
+		if isInitialLogin then
+			print("PLAYER_ENTERING_WORLD isInitialLogin")
+			self:Initialize()
+		elseif not isReloadingUi then -- zoning between instance maps
+			print("zoning")
+			self:Remorph()
+		end
+	end)
+end
+
+-- fires 2 times when shapeshifting into a form and 3 times when shifting back
+function iMorphLua:UPDATE_SHAPESHIFT_FORM()
+	local form, formid = GetShapeshiftForm(), GetShapeshiftFormID()
+	Spew("UPDATE_SHAPESHIFT_FORM", GetTime(), form, formid)
+	shapeshifted = true
+end
+
+-- usually fires right after UPDATE_SHAPESHIFT_FORM
+function iMorphLua:UNIT_MODEL_CHANGED(target)
+	Spew("UNIT_MODEL_CHANGED", GetTime(), target, shapeshifted)
+	if shapeshifted then
+		-- morphing triggers UNIT_MODEL_CHANGED again, avoid an infinite loop
+		shapeshifted = false
+		local form, formid = GetShapeshiftForm(), GetShapeshiftFormID()
+		--print(form, formid, state.form[form])
+		if form and state.form[form] then
+			print("morphed to form", form, formid)
+			Morph(state.form[form])
+		elseif form == 0 and state.morph then
+			print("morphed back to humanoid form", form, formid)
+			Morph(state.morph)
+		end
+	end
+end
+
+tinsert(CM.db_callbacks, function()
+	db = ClickMorphDB
+	db.version = VERSION
+	state = db.state
+	state.form = state.form or {}
+end)
+
+-- when imorph is injecting
+function iMorphLua:Initialize()
+	print("iMorphLua:Initialize")
+	if Morph then
+		print("iMorphLua:Initialize remorphing")
+		self:Remorph()
+		-- UPDATE_SHAPESHIFT_FORM can fire before imorph is registered
+		if canShapeshift then
+			iMorphLua:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
+		end
+		iMorphLua:RegisterEvent("UNIT_MODEL_CHANGED")
+	end
+end
+
+-- gets called when imorph injects and on /reload
+function iMorphLua:OnInject()
+	if time() > initTime + 1 then
+		print("iMorphLua:OnInject")
+		self:Remorph()
+	end
+end
+
+function iMorphLua:Remorph()
+	print("iMorphLua:Remorph")
+	if Morph then
 		if state.race or state.sex then
 			local race = state.race or select(3, UnitRace("player"))
 			local sex = state.sex or UnitSex("player")-1
 			SetRace(race, sex)
 		end
 		if state.morph then
+			--print("morph", state.morph)
 			Morph(state.morph)
 		end
 		if state.scale then
 			SetScale(state.scale)
 		end
-	end)
+	end
 end
-
-iMorphLua:RegisterEvent("PLAYER_ENTERING_WORLD")
-iMorphLua:SetScript("OnEvent", iMorphLua.OnEvent)
-
-tinsert(CM.db_callbacks, function()
-	db = ClickMorphDB
-	state = db.state
-end)
-
-local EnchantSlots = {
-	[1] = INVSLOT_MAINHAND,
-	[2] = INVSLOT_OFFHAND,
-}
 
 function iMorphLua:Reset()
 	SetRace(select(3, UnitRace("player")), UnitSex("player")-1)
@@ -123,7 +202,14 @@ local commands = {
 		id = tonumber(id)
 		if id then
 			Morph(id)
-			state.morph = id
+			if canShapeshift then
+				local form = GetShapeshiftForm()
+				if form > 0 then
+					state.form[form] = id
+				end
+			else
+				state.morph = id
+			end
 			state.race, state.sex = nil, nil
 		end
 	end,
@@ -222,11 +308,13 @@ local commands = {
 			SetWeather(id, intensity)
 		end
 	end,
+	--[[
 	-- smart morphing
 	shapeshift = function(form, displayID)
 		form, displayID = tonumber(form), tonumber(displayID)
 		if form and displayID then
-			SetShapeshiftForm(form, displayID)
+			--SetShapeshiftForm(form, displayID)
+			state.form[form] = displayID
 		end
 	end,
 	enablesm = function()
@@ -235,6 +323,7 @@ local commands = {
 	disablesm = function()
 		SetFlag(FLAG_SMARTMORPH, 0)
 	end,
+	]]
 }
 
 local SendText = ChatEdit_SendText
